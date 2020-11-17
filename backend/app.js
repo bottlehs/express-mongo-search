a68828b2002fc1821bd2cc7736d8877599c3f3cb
bottlehs <qudgns9@gmail.com>
2020-11-17 17:19:30 +0900

    router 추가

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var employeesRouter = require('./routes/employees');
 
 // mongo
 var mongooese = require('mongoose');
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/employees', employeesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/routes/employees.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var employee = require('../controllers/EmployeeController.js');
+const { route } = require('./index.js');
+
+router.get('/', employee.list);
+
+router.get('/show/:id', employee.show);
+
+router.get('/create', employee.create);
+
+router.post('/save', employee.save);
+
+router.get('/edit/:id', employee.edit);
+
+router.post('/update/:id', employee.update);
+
+router.post('/delete/:id', employee.delete);
+
+module.exports = router;
